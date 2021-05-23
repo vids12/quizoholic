6b9c75c520216b495fae04e8b708b5a98273753c
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-05-23 23:27:48 +0530

    added: nav-bar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,5 @@
+export default function Navbar() {
+  return <nav className="bg-indigo-600 md:h-14 h-20 rounded-bl-lg">
+    <p className="text-white text-3xl p-2 font-extrabold">Quizoholic</p>
+  </nav>
+}
