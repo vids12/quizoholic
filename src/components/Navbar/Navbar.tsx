82a1d2d253d0fa229eedaec990a9f95677c32465
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-07-23 17:04:42 +0530

    remove: div

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
 
   return <nav className="bg-indigo-600 md:h-14 h-20 rounded-bl-lg flex justify-between items-center relative">
     <Link to="/" className="text-white text-3xl p-2 font-extrabold cursor-pointer" onClick = {()=> scoreDispatch({type: "NIL_SCORE"})}>Quizoholic</Link>
-    <div><i className="fa fa-user-circle text-5xl text-white cursor-pointer p-2 mr-4 inline-block" aria-hidden="true" onClick={() => setUser(!user)}></i></div>
+    <i className="fa fa-user-circle text-5xl text-white cursor-pointer p-2 mr-4 inline-block" aria-hidden="true" onClick={() => setUser(!user)}></i>
     {user && <Dropbox />}
   </nav>
 }
