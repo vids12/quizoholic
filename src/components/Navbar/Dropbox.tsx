ecdd19a20acdfb356413481acc24b404fbe30594
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-07-23 17:56:20 +0530

    fix: fontAwesome CORS error

--- src/components/Navbar/Dropbox.tsx
+++ src/components/Navbar/Dropbox.tsx
@@ -16,7 +16,7 @@ export function Dropbox() {
       }
     return (
         <div className="flex flex-col absolute right-0 md:top-14 bg-indigo-800 top-20 text-white font-semibold p-4 md:p-6 rounded-lg z-10 w-3/5 md:w-1/5">
-            <p className="font-bold text-lg md:font-extrabold p-2 md:p-4">
+            <p className="font-bold text-lg md:font-extrabold p-1 md:p-3 w-min">
                 {loginStatus ? `Signed in as ${currentUser?.email}` : "Log In first"}
             </p>
             <hr />
