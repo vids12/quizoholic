9ff7fd243de31c3ddae008925d1ecde3622a27f8
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-08-02 23:14:11 +0530

    add: space

--- src/components/Rules/List.tsx
+++ src/components/Rules/List.tsx
@@ -7,11 +7,11 @@ export function List() {
       <li>There are 5 questions on India's {categoryName}.</li>
       <li>
         For every correct answer, 5 points are
-        <span className="text-green-500">granted</span>.
+        <span className="text-green-500"> granted</span>.
       </li>
       <li>
         For every incorrect answer, 2 points will be
-        <span className="text-red-600">deducted</span>.
+        <span className="text-red-600"> deducted</span>.
       </li>
       <li>You can skip the question.</li>
     </ul>
