acc39ed07eec1c8594c5f53891d2765b037890fe
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-08-02 14:38:49 +0530

    fix : minor

--- src/components/Auth/UserGrid.tsx
+++ src/components/Auth/UserGrid.tsx
@@ -1,57 +1,101 @@
-import { useEffect, useState } from "react";
-import { useAuth } from "../../dataProvider/context/Auth/AuthProvider"
-import { userData } from "../../firebase/firebase";
-import { UserData } from "./UserData.types";
+import { useEffect, useState } from 'react'
+import { useAuth } from '../../dataProvider/context/Auth/AuthProvider'
+import { userData } from '../../firebase/firebase'
+import { UserData } from './UserData.types'
 
-export function USerGrid(){
-    const { currentUser } = useAuth();
-    const [showData,setShowData] = useState<UserData>();
-    useEffect(()=>{
-        let isCanceled = false;
-        (async function () {
-            try{
-                if(!isCanceled){
-                    const response =await userData.doc(currentUser?.uid).get();
-                    setShowData({
-                        food: response.get("food"),
-                        heritage: response.get("heritage"),
-                        history: response.get("history"),
-                        literature: response.get("literature"),
-                        dance: response.get("dance"),
-                        geography: response.get("geography"),
-                    });
-                }
-            }catch(error){
-                if(!isCanceled){
-                console.error(error.message);
-                }
-            }      
-        })();
-        return () => {
-            isCanceled = true;
+export function UserGrid() {
+  const { currentUser } = useAuth()
+  const [showData, setShowData] = useState<UserData>()
+  useEffect(() => {
+    let isCanceled = false
+    ;(async function () {
+      try {
+        if (!isCanceled) {
+          const response = await userData.doc(currentUser?.uid).get()
+          setShowData({
+            food: response.get('food'),
+            heritage: response.get('heritage'),
+            history: response.get('history'),
+            literature: response.get('literature'),
+            dance: response.get('dance'),
+            geography: response.get('geography'),
+          })
         }
-    })
-    return <div className="grid grid-cols-3 grid-rows-6 m-6 bg-indigo-900">
-        <div className="font-bold rounded p-2 uppercase text-center m-1 bg-indigo-700 text-gray-100" >Categories</div>
-        <div className="font-bold rounded p-2 uppercase text-center m-1 bg-indigo-700 text-gray-100" >Attempted</div>
-        <div className="font-bold rounded p-2 uppercase text-center m-1 bg-indigo-700 text-gray-100">Highest Score</div>
-        <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1">Dance</div>
-        <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">{showData?.dance.attempted}</div>
-        <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">{showData?.dance.highestScore}</div>
-        <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1">Food</div>
-        <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">{showData?.food.attempted}</div>
-        <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">{showData?.food.highestScore}</div>
-        <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1">Geography</div>
-        <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">{showData?.geography.attempted}</div>
-        <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">{showData?.geography.highestScore}</div>
-        <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1">Heritage</div>
-        <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">{showData?.heritage.attempted}</div>
-        <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">{showData?.heritage.highestScore}</div>
-        <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1">History</div>
-        <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">{showData?.history.attempted}</div>
-        <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">{showData?.history.highestScore}</div>
-        <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1">Literature</div>
-        <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">{showData?.literature.attempted}</div>
-        <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">{showData?.literature.highestScore}</div>
-    </div>  
+      } catch (error) {
+        if (!isCanceled) {
+          console.error(error.message)
+        }
+      }
+    })()
+    return () => {
+      isCanceled = true
+    }
+  })
+  return (
+    <div className="grid grid-cols-3 grid-rows-6 m-6 bg-indigo-900">
+      <div className="font-bold rounded p-2 uppercase text-center m-1 bg-indigo-700 text-gray-100">
+        Categories
+      </div>
+      <div className="font-bold rounded p-2 uppercase text-center m-1 bg-indigo-700 text-gray-100">
+        Attempted
+      </div>
+      <div className="font-bold rounded p-2 uppercase text-center m-1 bg-indigo-700 text-gray-100">
+        Highest Score
+      </div>
+      <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1">
+        Dance
+      </div>
+      <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">
+        {showData?.dance.attempted}
+      </div>
+      <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">
+        {showData?.dance.highestScore}
+      </div>
+      <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1">
+        Food
+      </div>
+      <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">
+        {showData?.food.attempted}
+      </div>
+      <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">
+        {showData?.food.highestScore}
+      </div>
+      <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1">
+        Geography
+      </div>
+      <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">
+        {showData?.geography.attempted}
+      </div>
+      <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">
+        {showData?.geography.highestScore}
+      </div>
+      <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1">
+        Heritage
+      </div>
+      <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">
+        {showData?.heritage.attempted}
+      </div>
+      <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">
+        {showData?.heritage.highestScore}
+      </div>
+      <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1">
+        History
+      </div>
+      <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">
+        {showData?.history.attempted}
+      </div>
+      <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">
+        {showData?.history.highestScore}
+      </div>
+      <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1">
+        Literature
+      </div>
+      <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">
+        {showData?.literature.attempted}
+      </div>
+      <div className="rounded m-1 text-lg text-gray-200 bg-indigo-400 p-1 text-right">
+        {showData?.literature.highestScore}
+      </div>
+    </div>
+  )
 }
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,14 +1,16 @@
-import { USerGrid } from "../components/Auth/UserGrid";
-import { useAuth } from "../dataProvider/context/Auth/AuthProvider";
+import { UserGrid } from '../components/Auth/UserGrid'
+import { useAuth } from '../dataProvider/context/Auth/AuthProvider'
 
-export function Profile(){
-    const { currentUser }: any= useAuth();
-    return <div>
-        <h4 className="text-center text-indigo-600 text-4xl m-2 tracking-widest">
-            My Profile
-        </h4>
-        <p className="text-lg m-4">Name: {currentUser?.displayName}</p>
-        <p className="text-lg m-4">Email: {currentUser?.email}</p>
-        <USerGrid />
+export function Profile() {
+  const { currentUser }: any = useAuth()
+  return (
+    <div>
+      <h4 className="text-center text-indigo-600 text-4xl m-2 tracking-widest">
+        My Profile
+      </h4>
+      <p className="text-lg m-4">Name: {currentUser?.displayName}</p>
+      <p className="text-lg m-4">Email: {currentUser?.email}</p>
+      <UserGrid />
     </div>
+  )
 }
